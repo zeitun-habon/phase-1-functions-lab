2d68209df8fbe1499ae0d22f33df1c8df6835568
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-07 11:36:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue-42)
+  }
+  function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+   
+  }
+ 
