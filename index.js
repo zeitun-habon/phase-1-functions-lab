ed5ec356e97aac131fe937aa16d76f37741a9677
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-07 15:25:39 +0300

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ function distanceFromHqInBlocks(someValue) {
     return Math.abs(someValue-42)
   }
   function distanceFromHqInFeet(someValue) {
-    distanceFromHqInBlocks(someValue);
-   
+   let dis= distanceFromHqInBlocks(someValue)
+    return dis*264
+}
+function distanceTravelledInFeet(start, destination) {
+     
+     const distance= (destination-start)*264
+     
+    return Math.abs(distance)
   }
- 
+
+
+
+
+function calculatesFarePrice(start, destination) {
+    let distanceByFeet=distanceTravelledInFeet(start, destination)
+    if(distanceByFeet<=400){
+        return 0
+    }
+    else if(distanceByFeet>400 && distanceByFeet<=2000){
+        const priceInCents=(distanceByFeet-400)*2
+         return priceInCents/100
+    }
+    else if(distanceByFeet>=2000 && distanceByFeet<=2500){
+        return 25
+    }
+    else if(distanceByFeet>2500){
+        
+        return 'cannot travel that far'
+    }
+
+  }
